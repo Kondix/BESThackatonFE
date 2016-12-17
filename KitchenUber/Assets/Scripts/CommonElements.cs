1970d6787f41639cb27539ac2b401f6247a3a9e0
orlikb <orlik1212@gmail.com>
2016-12-17 19:07:49 +0100

    commit

--- KitchenUber/Assets/Scripts/CommonElements.cs
+++ KitchenUber/Assets/Scripts/CommonElements.cs
@@ -17,7 +17,7 @@ public class CommonElements : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
     public CommonElements GetInstance()
     {
---
+++ KitchenUber/Assets/Scripts/StartPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPanel : MonoBehaviour {
+
+    public GameObject startPanel;
+    public GameObject yourProfile;
+
+         
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void LogInButtonMethod()
+    {
+        startPanel.SetActive(false);
+        yourProfile.SetActive(true);
+    }
+    public void CookingButtonMethon()
+    {
+        SceneManager.LoadScene("CookingToday");     
+    }
+    public void HungryButtonMethod()
+    {
+
+    }
+
+
+}
